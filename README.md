3b433aac4a42b4466d3b1234a29b08967aae6842
orozcoding <orozcoding@gmail.com>
2021-12-18 09:36:46 -0400

    Clone repo

---
+++ README.md
@@ -0,0 +1,32 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Orozcoding's Portfolio
+
+> Personal Portfolio Webpage showing my (future) projects.
+
+## Preview
+
+![Preview Mobile](img/previewmobile.png)
+![Preview Desktop](img/previewdesktop.png)
+
+## Live Demo
+
+Visit My Portfolio with the following link:
+[My Portfolio Website](https://orozcoding.github.io/myportfolio/)
+
+## Built With
+
+- HTML
+- CSS
+- javaScript
+
+## Authors
+
+👤 **Author**
+
+- GitHub: [@orozCoding](https://github.com/orozCoding)
+- Twitter: [@orozCoding](https://twitter.com/orozCoding)
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ js/form.js
@@ -0,0 +1,47 @@
+const nameInput = document.getElementById('form-name');
+const emailInput = document.getElementById('form-email');
+const msgInput = document.getElementById('form-msg');
+const errorMsg = document.getElementsByClassName('form-error');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    event.preventDefault();
+    errorMsg[1].innerHTML = 'Please use lower case.';
+    emailInput.style.border = 'solid 5px red';
+  } else {
+    errorMsg[1].innerHTML = '';
+    emailInput.style.border = null;
+  }
+});
+
+let formStorage = {};
+
+if (!localStorage.getItem('formInfo')) {
+  formStorage = {
+    formName: '',
+    formEmail: '',
+    formMsg: '',
+  };
+  localStorage.setItem('formInfo', JSON.stringify(formStorage));
+} else if (localStorage.getItem('formInfo')) {
+  formStorage = JSON.parse(localStorage.getItem('formInfo'));
+  nameInput.value = formStorage.formName;
+  emailInput.value = formStorage.formEmail;
+  msgInput.value = formStorage.formMsg;
+}
+
+nameInput.addEventListener('change', () => {
+  formStorage.formName = nameInput.value;
+  localStorage.setItem('formInfo', JSON.stringify(formStorage));
+});
+
+emailInput.addEventListener('change', () => {
+  formStorage.formEmail = emailInput.value;
+  localStorage.setItem('formInfo', JSON.stringify(formStorage));
+});
+
+msgInput.addEventListener('change', () => {
+  formStorage.formMsg = msgInput.value;
+  localStorage.setItem('formInfo', JSON.stringify(formStorage));
+});
